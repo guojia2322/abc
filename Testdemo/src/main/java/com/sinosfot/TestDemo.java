99e000c8ce0cd2cc9b282c52a958aedecf79d420
guojia <jia1324981330@163.com>
2019-04-30 11:42:04 +0800

    测试快捷

--- Testdemo/src/main/java/com/sinosfot/TestDemo.java
+++ Testdemo/src/main/java/com/sinosfot/TestDemo.java
@@ -6,6 +6,7 @@ package com.sinosfot;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println(2222233); System.out.println(2222233);
+        System.out.println(3333);
 
 
     }
